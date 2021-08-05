f1064208199abb98225fdd4484becf0d0ae4ab54
lchaudhry <lchaudhry-NONEMP@collegeboard.org>
2021-08-05 17:57:23 -0400

    Updated tests

--- helpers/PaginationHelper.java
+++ helpers/PaginationHelper.java
@@ -1,5 +1,7 @@
 package helpers;
 
+import org.junit.Assert;
+
 public class PaginationHelper
 {
     String[] stringListSet;
@@ -13,11 +15,13 @@ public class PaginationHelper
 
     public int pageCount()
     {
+        validateValidStringList(stringListSet);
         return (int) Math.ceil(stringListSet.length / itemsPerPageSet); // Ceil rounds up regardless of number, 1.2 -> 2
     }
 
     public int itemCount()
     {
+        validateValidStringList(stringListSet);
         return stringListSet.length;
     }
 
@@ -41,7 +45,8 @@ public class PaginationHelper
     public int pageItemCount(int pageNumber)
     {
         // subtracting by 1 here due to zero-based index
-        if (pageNumber < pageCount() - 1) // if specified page is not last, then it is max # allowed per page
+        if (pageNumber >= 0 && pageNumber < pageCount() - 1) // if specified page is not last, then it is max # allowed
+            // per page
         {
             return (int) itemsPerPageSet;
         }
@@ -49,17 +54,27 @@ public class PaginationHelper
         {
             return itemCountOnLastPage();
         }
+        System.out.println("Page number must be less than or equal to " + (pageCount() - 1) + "(zero indexing)");
         return -1; // only occurs if page number is greater than possible pages
     }
 
     public int pageIndex(int index)
     {
-        if (index < 0 || index > itemCount()) // if index is negative or greater than item count, we throw -1 eerror
+        if (index <= 0 || index > itemCount()) // if index is negative or greater than item count, we throw -1 error
         {
+            System.out.println("Page index must be greater than 0 and less than total item count");
             return -1;
         }
 
         return (int) Math.ceil(index / itemsPerPageSet) - 1; // otherwise, we simply divide item index by maxPage set
         // and always round up. We minus 1 due to 0-based indexing
+    }
+
+    public void validateValidStringList(String[] stringList)
+    {
+        if (stringList == null || stringList.length == 0)
+        {
+            Assert.fail("List is empty. Please input a valid list.");
+        }
     }
 }
--- test/paginationTests/PaginationTests.java
+++ test/paginationTests/PaginationTests.java
@@ -1,9 +1,8 @@
 package paginationTests;
 
+import helpers.PaginationHelper;
 import org.junit.Assert;
 import org.junit.Test;
-
-import helpers.PaginationHelper;
 
 public class PaginationTests
 {
@@ -26,15 +25,71 @@ public class PaginationTests
     {
         Assert.assertEquals(4, helper.pageItemCount(0));
         Assert.assertEquals(2, helper.pageItemCount(1));
+    }
+
+    @Test
+    public void validatePageItemCount_OutOfBounds()
+    {
         Assert.assertEquals(-1, helper.pageItemCount(2));
+    }
+
+    @Test
+    public void validateNegativePageNumber()
+    {
+        Assert.assertEquals(-1, helper.pageItemCount(-1));
     }
 
     @Test
     public void validatePageIndex()
     {
-        Assert.assertEquals( 1, helper.pageIndex(5));
+        Assert.assertEquals(1, helper.pageIndex(5));
         Assert.assertEquals(0, helper.pageIndex(2));
         Assert.assertEquals(-1, helper.pageIndex(20));
+    }
+
+    @Test
+    public void validatePageIndexOfZero()
+    {
+        Assert.assertEquals(-1, helper.pageIndex(0));
+    }
+
+    @Test
+    public void validateNegativePageIndex()
+    {
         Assert.assertEquals(-1, helper.pageIndex(-10));
     }
+
+    @Test
+    public void stringListIsProperlyValidatedOnPageCount()
+    {
+        helper = new PaginationHelper(null, 2);
+        try
+        {
+            helper.pageCount();
+            throw new AssertionError("Error was not thrown.");
+        }
+        catch (AssertionError e)
+        {
+            Assert.assertEquals("List is empty. Please input a valid list.", e.getMessage());
+        }
+
+    }
+
+    @Test
+    public void stringListIsProperlyValidatedOnItemCount()
+    {
+        helper = new PaginationHelper(null, 2);
+        try
+        {
+            helper.itemCount();
+            throw new AssertionError("Error was not thrown.");
+        }
+        catch (AssertionError e)
+        {
+            Assert.assertEquals("List is empty. Please input a valid list.", e.getMessage());
+        }
+
+    }
+
+
 }
